4fcd8ffa8d8af52bba0711191636913a6adf2794
Duykiller04 <haijano04@gmail.com>
2024-09-05 16:34:19 +0700

    Update code

--- resources/views/client/layouts/partials/header-nav.blade.php
+++ resources/views/client/layouts/partials/header-nav.blade.php
@@ -2,13 +2,13 @@
     <div class="row">
         <div class="col-lg-3 col-md-3">
             <div class="header__logo">
-                <a href="./index.html"><img src="/client/img/logo.png" alt=""></a>
+                <a href="{{ route('home') }}"><img src="/client/img/logo.png" alt=""></a>
             </div>
         </div>
         <div class="col-lg-6 col-md-6">
             <nav class="header__menu mobile-menu">
                 <ul>
-                    <li class="active"><a href="{{ route('index') }}">Home</a></li>
+                    <li class="active"><a href="{{ route('home') }}">Home</a></li>
                     <li><a href="{{ route('shop') }}">Shop</a></li>
                     {{-- <li><a href="#">Pages</a>
                         <ul class="dropdown">
--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[ProductController::class, 'index'])->name('index');
-Route::get('/admin',function(){
-    return 'Day la admin';
-});
+Route::get('/',[ProductController::class, 'index'])->name('home');
+// Route::get('/admin',function(){
+//     return 'Day la admin';
+// });
 //home
 Route::get('about',[HomeController::class, 'about'])->name('about');
 
@@ -50,3 +50,6 @@ Route::post('order/save', [OrderController::class, 'save'])->name('order.save');
 //shop
 Route::get('shop/{id?}',[ShopController::class, 'shop'])->name('shop');
 Route::post('filter',[ShopController::class, 'filter'])->name('filter');
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
