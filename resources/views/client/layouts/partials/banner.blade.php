bd0942bc1dc788839c1dafddc7db4a51e5d513c4
Khaicao2004 <khaicao2004@gmail.com>
2024-09-07 10:31:02 +0700

    thống kê

--- resources/views/client/layouts/partials/banner.blade.php
+++ resources/views/client/layouts/partials/banner.blade.php
@@ -1,34 +1,21 @@
     <!-- Hero Section Begin -->
     <section class="hero">
         <div class="hero__slider owl-carousel">
-            @foreach ($products as $product)
-                @php
-                    $url = $product->img_thumbnail;
-                    if (!Str::contains($url, 'http')) {
-                        $url = Storage::url($url);
-                    }
-                @endphp
-                <div class="hero__items set-bg" data-setbg="{{$url}}">
+                <div class="hero__items set-bg" data-setbg="/client/img/banner/kieuloan.jpg">
                     <div class="container">
                         <div class="row">
                             <div class="col-xl-5 col-lg-7 col-md-8">
                                 <div class="hero__text">
-                                    <h6>{{ $product->catalogue->name }}</h6>
-                                    <h2>{{ $product->name }}</h2>
-                                    <p>{{ $product->description }}</p>
-                                    <a href="{{ route('product.detail', $product->slug) }}" class="primary-btn">Mua ngay<span class="arrow_right"></span></a>
+                                    <h2>Kiều Loan Shop</p>
+                                    <a href="{{ route('shop') }}" class="primary-btn">Mua ngay<span class="arrow_right"></span></a>
                                     <div class="hero__social">
-                                        <a href="#"><i class="fa fa-facebook"></i></a>
-                                        <a href="#"><i class="fa fa-twitter"></i></a>
-                                        <a href="#"><i class="fa fa-pinterest"></i></a>
-                                        <a href="#"><i class="fa fa-instagram"></i></a>
+                                        <a href="https://www.facebook.com/profile.php?id=100063703743442"><i class="fa fa-facebook" style="font-size: 40px"></i></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            @endforeach
         </div>
     </section>
     <!-- Hero Section End -->
